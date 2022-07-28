e7b9bde800fbfef2f6c798e4f00db3649883531e
mrinalinichava <30821840+mrinalinichava@users.noreply.github.com>
2022-07-28 18:04:24 +0530

    initial commit

---
+++ src/main/java/ExampleMain.java
@@ -0,0 +1,12 @@
+public class ExampleMain {
+
+    public static void main(String[] args) {
+
+        System.out.println("RUNNING FROM THE PACKAGE_JAR");
+    }
+
+    public static void someFunction()
+    {
+        System.out.println("INVOKED SOME FUNCTION IN JAR");
+    }
+}
